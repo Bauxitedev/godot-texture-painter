82331adbfb1a47c2b43bf9806f996cc31fb6f485
Bauxitedev <31383123+Bauxitedev@users.noreply.github.com>
2018-05-16 11:14:33 +0200

    Add bilinear filter + anisotropic filter to the paint textures to get smooth jaggies

--- views/Viewport/Viewport.gd
+++ views/Viewport/Viewport.gd
@@ -16,13 +16,8 @@ func _ready():
 	state_machine.switch_state("Paint")
 	
 	change_mesh(preload("res://assets/models/Suzanne.mesh"))
-	
-	# For debugging so you can see this works...
-	# yield(get_tree().create_timer(2.0), "timeout")
-	# change_mesh(preload("res://assets/models/Torus.mesh"))
-	
-	# yield(get_tree().create_timer(2.0), "timeout")
-	# change_mesh(preload("res://assets/models/Suzanne.mesh"))
+
+
 
 func _on_softness_slider_value_changed(value):
 	
@@ -37,10 +32,22 @@ func change_mesh(mesh):
 	# This will make the program paint on a different mesh
 	
 	# TODO rename the "suz" node to "mesh" for consistency
-	# Hack - setting a mesh resets the material, so keep it around and set it again
 	var mat = $spatial/suz.get_surface_material(0)
 	$spatial/suz.mesh = mesh
+
+	# Set all the viewports to Filter + Aniso so we get smooth jaggies
+	# (This needs to be done here, since it seems not to work when set in the editor)
+	var flags = Texture.FLAG_FILTER | Texture.FLAG_ANISOTROPIC_FILTER
+	# Don't enable REPEAT here or the seams will probably get worse
+	
+	mat.albedo_texture.flags = flags
+	mat.roughness_texture.flags = flags	
+	mat.metallic_texture.flags = flags		
+	mat.emission_texture.flags = flags
+	
 	$spatial/suz.set_surface_material(0, mat)
+	
+
 	
 	# Regenerate all the mesh textures
 	for vp in $textures/mesh.get_children():
