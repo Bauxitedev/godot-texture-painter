455b5d1b5cc60b6a03b16ee1dfe0eb87c5e5bcd9
Bauxitedev <31383123+Bauxitedev@users.noreply.github.com>
2018-07-21 14:47:57 +0200

    Dialogs is now a CanvasLayer to draw on top of everything else without changing tree order

--- autoloads/Dialogs.gd
+++ autoloads/Dialogs.gd
@@ -1,11 +1,10 @@
-extends Node
+extends CanvasLayer
 
 # This is purely a container for Dialogs.
 # It can be used to detect if there are any open dialogs at the moment.
 func _ready():
-	yield(get_tree(), "idle_frame")
-	get_parent().move_child(self, get_parent().get_children().size()-1) # Little hack to ensure the Dialogs is the last singleton in the tree so it's drawn on top of everything else
-		
+	layer = 1 # Draw on top of everything else
+
 func any_dialog_open():
 	for c in get_children():
 		if c.visible:
